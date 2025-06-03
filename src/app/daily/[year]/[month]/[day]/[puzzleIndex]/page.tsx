ace4a75625740de4d4ccffedb11a3e194e695de4
michael <michaelslack42@gmail.com>
2025-06-03 23:04:35 +0100

    undo exp

--- src/app/daily/[year]/[month]/[day]/[puzzleIndex]/page.tsx
+++ src/app/daily/[year]/[month]/[day]/[puzzleIndex]/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 
 type RouteParams = {
   year: number;
-  month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  month: number;
   day: number;
   puzzleIndex: number;
 };
