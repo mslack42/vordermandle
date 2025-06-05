34c2285e91ff36eb27ab36799ba819641c45ef11
michael <michaelslack42@gmail.com>
2025-06-05 22:58:23 +0100

    Add construction icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -15,7 +14,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Vordermandle",
-  description: "A daily puzzle game",
+  description: "A Countdown Wordlelike",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
